9a80309f502662464beff1553ac563c6af260aaf
cyle <cyle@standingdesk3>
2013-08-07 16:09:32 -0400

    - finished first version of the parser, updated README to match

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# captain's log format
+# captain's log parser
 
-## overall
+## what is this?
 
-save this as a plain text .txt file, any name, whatever. my convention is to have the filename be the date, in the same form as the first line, i.e. 2013-08-07.txt
+I keep track of what I do every day, and I do that in text files. One for every day. Until recently, this has been good enough. It's like a daily journal: a folder full of text files, each representing one day of work.
 
-the parser will ignore any file that isn't .txt, and will ignore any .txt file that doesn't have the date as the first line.
+But how do I get data out of that? Thankfully, I keep a very standard (but somewhat flexible) format to my text files.
 
-if there are files that duplicate dates (i.e. you have two text files that start with 2013-04-12), the parser will throw an error.
+This is a parser for those text files. The parser goes through a given directory, finds text files, and parses them into one big data object if the file is of the right format. That format is outlined below.
+
+## parser usage
+
+Simply run `./parser.php name_of_directory/` where that directory path name is where your text files are stored. That's it!
+
+The parser does not modify your files in any way, it just reads them.
+
+## overall format considerations
+
+save the log file as a plain text .txt file, any name, whatever. my convention is to have the filename be the date, in the same form as the first line, i.e. 2013-08-07.txt, but you can do whatever.
+
+the parser will ignore any file that isn't .txt, and will ignore any .txt file that doesn't have the date as the first line. any spaces at the beginning or ends of lines will be parsed out.
+
+if there are files that duplicate dates (i.e. you have two text files that start with 2013-04-12 on the first line), the parser will throw an error.
 
 ## the file format spec
 
-first line: the day's date, format: YYYY-MM-DD
+first line: the day's date in format: YYYY-MM-DD
 
 third line: "Worked on..." or "Worked on:" or "Worked on" (case insensitive)
 
@@ -18,14 +32,14 @@ lines 5-n: a list of lines, each line being an item you worked on that day
 
 for each of those items, each on a single line, you can include the following:
 
-- if you put a dash (-) and then a space, the parser will ignore it.
-- the time spent doing the thing in brackets, i.e. [1 hour] or [1h30m] or [9:00am - 1:00pm] or [9am to 11am]
+- if you put a dash (-) and then a space at the start of the line, the parser will ignore it. it just makes the file easier to read for you.
+- the time spent doing the thing, in brackets, for time tracking. examples: [1 hour] or [1h30m] or [9:00am - 1:00pm] or [9am to 11am]
 - a footprints ticket ID or project ID associated with it, enclosed in curly braces, the first number being the workspace and the second number being the ticket number i.e. {1#6112} is Help Desk workspace ticket ID 6112
 - if you include "meeting" in the line item, it'll count as a meeting
 
 when you're done listing out what you've done, add a blank new line
 
-line n+2: "Notes..." or "Notes:" or "Notes" (case insensitive) (optional, you don't need to have this.) 
+line n+2: "Notes..." or "Notes:" or "Notes" (case insensitive) (optional, you don't need to have a notes section at all.) 
 
 lines n+4 and beyond: whatever additional notes you want to add about the day. if you write just "welp" or "welp." that means you have no notes. (optional, you don't need to have this at all, you can leave it blank.)
 
@@ -42,5 +56,8 @@ lines n+4 and beyond: whatever additional notes you want to add about the day. i
     
     notes
     
-    today i was really bored.
-    
+    today I was really bored.
+
+## what you get at the end
+
+when it's all done parsing, you will have a massive PHP array of everything you did, how much time it took per item, how much time you spent working per day, and how much time you spent working over all of the logs, as well as how many meetings you attended per day or of all time.
--- parser.php
+++ parser.php
@@ -10,4 +10,160 @@ if (php_sapi_name() != 'cli') {
 
 echo "Captain's Log Parser! \n";
 
+if (!isset($argv) || count($argv) < 2) {
+	die('You need to specify a directory to parse as the argument to this script.'."\n");
+}
+
+if (in_array($argv[1], array('-h', '-help', '--help'))) {
+	echo 'Usage:   just call this script and have the first argument'."\n".'         be the folder full of txt files you\'d like parsed.'."\n";
+	echo 'Example: ./parser.php directory_of_txts/'."\n";
+	die();
+}
+
+$all_the_logs = array();
+
+$dir = trim($argv[1]);
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+			if ($file == '.' || $file == '..' || substr($file, 0, 1) == '.') {
+				continue; // do not parse hidden files
+			}
+			if (substr($file, -4) != '.txt') {
+				continue; // only parse .txt files
+			}
+            echo 'file to parse: '.$file."\n";
+			$raw = file_get_contents($dir.'/'.$file); // get those file contents
+			$raw = trim($raw); // trim out any white space now
+			if (strpos("\r\n", $raw) !== false) { $raw = str_replace("\r", "", $raw); } // if the file line endings are WINDOWS based, get rid of em
+			$raw_lines = explode("\n", $raw); // break the file into lines
+			if (preg_match('/\d{4}-\d{2}-\d{2}/', trim($raw_lines[0])) !== 1) {
+				// if the first line of the file isn't the DATE as outlined in the format spec, skip this file
+				echo 'This file is in the wrong format, skipping!'."\n";
+				continue;
+			}
+			
+			// get the date this file belongs to
+			$thedate = strtotime(trim($raw_lines[0]));
+			if (!$thedate) {
+				die('Error parsing the date on the first line of this file: '.$file."\n");
+			}
+			$thedate_string = date('Y-m-d');
+			if (array_key_exists($thedate_string, $all_the_logs)) {
+				die('Oops, it looks like this date has already been parsed in a different file: '.$thedate_string."\n".'Crashing now.'."\n");
+			}
+			
+			// let's set up a temporary array to store the info
+			$day = array();
+			$day['did'] = array(); // array of stuff that was done
+			$day['notes'] = ''; // place to put notes
+			$day['total_time'] = 0; // time spent DOING THINGS!
+			$day['total_meetings'] = 0; // how many meetings did you go to?
+			
+			// okay, let's go through the lines
+			$at_the_list = false; // take note of when we get past the list and enter the notes section
+			for ($ln = 0; $ln < count($raw_lines); $ln++) {
+				$line = trim($raw_lines[$ln]);
+				//echo $line."\n";
+				if ($ln > 3 && !$at_the_list) {
+					// if we are past the third line, this is where the list should be!
+					if (in_array(strtolower($line), array('notes', 'notes...', 'notes:'))) {
+						// if the line is blank, that means we've reached the end of the list!
+						$at_the_list = true;
+					} else {
+						// parse the list item
+						if (substr($line, 0, 2) == '- ') { $line = substr($line, 2); }
+						if (trim($line) == '') { continue; }
+						$activity = array();
+						$activity['meeting'] = false;
+						$activity['time'] = null;
+						$activity['tickets'] = array();
+						//echo 'List item: '. $line . "\n";
+						// let's parse that line for any time info info
+						if (preg_match_all('/\[([^\]]+)\]/', $line, $time_matches) > 0) {
+							foreach ($time_matches[1] as $time_match) {
+								// deal with a potential amount of time spent doing something!
+								$time_match = trim($time_match); // get rid of white space
+								if (strpos($time_match, 'to') !== false || strpos($time_match, '-') !== false) { // check to see if it's a range
+									// okay, split it up
+									if (preg_match('/(.+)(-|to)(.+)/', $time_match, $time_range_matches) !== false) {
+										//print_r($time_range_matches);
+										$start_time_string = trim($time_range_matches[1]);
+										$end_time_string = trim($time_range_matches[3]);
+										$start_time_ts = strtotime('2013-01-01 '.$start_time_string);
+										$end_time_ts = strtotime('2013-01-01 '.$end_time_string);
+										$seconds_worked = $end_time_ts - $start_time_ts;
+										//echo $time_range_matches[0].' = '.$seconds_worked.' seconds'."\n";
+										$day['total_time'] += $seconds_worked;
+										$activity['time'] += $seconds_worked;
+									}
+								} else {
+									// it should just be a relative time amount, like 1 hour, or 1h30m
+									// change lone "h"s into "hours" /h(?![a-z]+)/ig, lone "m"s into "mins" /m(?![a-z]+)/gi
+									$relative_time_string = $time_match;
+									$relative_time_string = preg_replace('/h(?![a-z]+)/i', ' hours ', $relative_time_string);
+									$relative_time_string = preg_replace('/m(?![a-z]+)/i', ' mins ', $relative_time_string);
+									$start_time_ts = time();
+									$end_time_ts = strtotime('+'.$relative_time_string);
+									$seconds_worked = $end_time_ts - $start_time_ts;
+									//echo $time_match.' = '.$seconds_worked.' seconds'."\n";
+									$day['total_time'] += $seconds_worked;
+									$activity['time'] += $seconds_worked;
+								}
+							}
+							// now that we are done with the time, get rid of that stuff
+							$line = preg_replace('/\[([^\]]+)\]/', '', $line);
+						}
+						// okay, now see if there's any ticket info
+						if (preg_match_all('/\{(\d+)#(\d+)\}/', $line, $ticket_matches, PREG_SET_ORDER) > 0) {
+							foreach ($ticket_matches as $ticket_match) {
+								$activity['tickets'][] = array('ticket_id' => $ticket_match[2] * 1, 'workspace_id' => $ticket_match[1] * 1);
+							}
+							$line = preg_replace('/\{(\d+)#(\d+)\}/', '', $line);
+						}
+						if (stripos($line, 'meeting') !== false) {
+							$activity['meeting'] = true;
+							$day['total_meetings']++;
+						}
+						$activity['info'] = trim($line);
+						$day['did'][] = $activity;
+					}
+				} else if ($at_the_list) {
+					// the only thing that should be left are the notes
+					if (trim($line) == '') { continue; }
+					if (strtolower(trim($line)) == 'welp' || strtolower(trim($line)) == 'welp.') { continue; }
+					//echo 'After list: '. $line . "\n";
+					$day['notes'] .= $line;
+					if ($ln != count($raw_lines) - 1) {
+						$day['notes'] .= "\n\n"; // there's more coming, put in some whitespace
+					}
+				}
+			}
+			
+			// save the day to the big array of logs!
+			$all_the_logs[$thedate_string] = $day;
+        }
+        closedir($dh); // close the directory, we are DONE
+		
+		// go through all of the log entries and find some things
+		$all_the_logs['total_time'] = 0;
+		$all_the_logs['total_meetings'] = 0;
+		foreach ($all_the_logs as $log_entry) {
+			$all_the_logs['total_time'] += $log_entry['total_time'];
+			foreach ($log_entry['did'] as $list_entry) {
+				if ($list_entry['meeting'] == true) { $all_the_logs['total_meetings']++; }
+			}
+		}
+				
+		// okay, all parsed
+		print_r($all_the_logs); // show it off
+				
+    } else {
+		die('Error opening the directory!'."\n");
+	}
+} else {
+	die('The directory you provided does not seem to exist, sorry.'."\n");
+}
+
 ?>
